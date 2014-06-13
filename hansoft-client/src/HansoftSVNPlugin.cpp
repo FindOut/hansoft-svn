e8d3152bf69e3fa71fd20ed6e28ab1636456d5f2
Björn Arnelid <bjorn.arnelid@gmail.com>
2014-06-13 14:27:48 +0200

    Plugin should work (in theory) and created makefile

--- hansoft-client/src/HansoftSVNPlugin.cpp
+++ hansoft-client/src/HansoftSVNPlugin.cpp
@@ -26,7 +26,7 @@ HansoftSVNPlugin::~HansoftSVNPlugin() {
 
 void HansoftSVNPlugin::initialize(const void *data) {
 	session = HPMSdkSession::SessionOpen(hpm_str(""), 0, hpm_str(""), hpm_str(""), hpm_str(""), this, NULL, true, EHPMSdkDebugMode_Off, data, 0, hpm_str(""), HPMSystemString(), NULL);
-	dialogTexts.m_ButtonLabel = session->LocalizationCreateUntranslatedStringFromString("Submit"); // HPMUntranslatedString//hpm_str("Submit");
+	dialogTexts.m_ButtonLabel = session->LocalizationCreateUntranslatedStringFromString("Submit");
 	dialogTexts.m_CancelLabel = session->LocalizationCreateUntranslatedStringFromString("Cancel");
 	dialogTexts.m_InfoText = session->LocalizationCreateUntranslatedStringFromString("Select items associated with your svn commit");
 	dialogTexts.m_Title = session->LocalizationCreateUntranslatedStringFromString("Svn Commit");
