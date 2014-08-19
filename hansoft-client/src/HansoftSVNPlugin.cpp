beaee0b28cc42f7a6191db354bb3c545b1f40cb2
Björn Arnelid <bjorn.arnelid@gmail.com>
2014-08-19 16:13:04 +0200

    Including commit in communicationpacket

--- hansoft-client/src/HansoftSVNPlugin.cpp
+++ hansoft-client/src/HansoftSVNPlugin.cpp
@@ -105,6 +105,8 @@ public:
 	}
 
 	virtual void On_Callback(const HPMChangeCallbackData_CommunicationChannelPacketReceived &_Data) {
+		_debuglog << _Data.m_ToSessionID << " , "<< m_pSession->ResourceGetLoggedIn() << std::endl;
+		_debuglog.flush();
 		if(_Data.m_ToSessionID == m_pSession->ResourceGetLoggedIn()) {
 			displayDialog(_Data);
 			std::string str(_Data.m_Packet.m_Bytes.begin(),_Data.m_Packet.m_Bytes.end());
