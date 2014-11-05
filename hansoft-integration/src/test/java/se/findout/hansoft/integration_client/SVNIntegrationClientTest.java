98038579471797c63c376d75b1e74626ae440ea0
Fredrik Attebrant <fredrik.attebrant@gmail.com>
2014-11-05 09:28:46 +0100

    Fixes compilation errors, however, tests still needs to be fixed.

--- hansoft-integration/src/test/java/se/findout/hansoft/integration_client/SVNIntegrationClientTest.java
+++ hansoft-integration/src/test/java/se/findout/hansoft/integration_client/SVNIntegrationClientTest.java
@@ -2,6 +2,7 @@ package se.findout.hansoft.integration_client;
 
 import java.io.IOException;
 
+import se.findout.hansoft.integration_server.IntegrationServer;
 import se.findout.hansoft.integration_server.adapter.Credentials;
 import se.findout.hansoft.integration_server.adapter.HansoftAdapter;
 import se.findout.hansoft.integration_server.adapter.HansoftException;
@@ -16,11 +17,14 @@ public class SVNIntegrationClientTest {
     public static void main(String [] args) throws HansoftException, IOException {
         HansoftAdapter adapter = HansoftAdapter.getInstance();
         HansoftServer server = new HansoftServer();
+        IntegrationServer is = new IntegrationServer();
         Credentials sdkUser = new Credentials();
         String sdkUsername = System.getenv("HANSOFT_SDK_USERNAME");
         String sdkPassword = System.getenv("HANSOFT_SDK_PASSWORD");
         String sdkDatabase = System.getenv("HANSOFT_DATABASE");
         String testUserName = System.getenv("TEST_USERNAME");
+        String propertyFile = System.getenv("PROPERTY_FILE");
+        IntegrationServer.loadProperties(propertyFile);
         sdkUser.setUsername(sdkUsername);
         sdkUser.setPassword(sdkPassword);
         adapter.initialize(server, sdkDatabase, sdkUser);
@@ -34,9 +38,10 @@ public class SVNIntegrationClientTest {
         System.out.println("session id " + id);
         String author = "tester";
         int revision = 42;
+        String timestamp = "2014-10-29 14:56:38 +0100 (Wed, 29 Oct 2014)";
         String path = "/a/b/c/repo/dir";
         String message = "Test of commit message";
-        Commit commit = new Commit(author, revision, path, message);
+        Commit commit = new Commit(author, revision, path, timestamp, message);
         adapter.signalCommitPerformed(id, commit);
         System.out.println("Packet sent!");
     }
